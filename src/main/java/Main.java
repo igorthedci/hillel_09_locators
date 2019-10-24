2e6699cfff876401b494bc42057df211aedc70b2
igorthedci <foumart@ukr.net>
2019-10-24 14:41:54 +0300

    Homework 09. XPath/CSS Locators (completed)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,8 +17,8 @@ public class Main {
         String xpathContactTitle = "//*[@id='contact-link']";
         String xpathSignInTitle = "//a[@class='login']";
         String cssTopBanner = "img[src*='banner']";
-        String cssShopPhoneTitle = "";
-        String cssShopPhoneNumber = "";
+        String cssShopPhoneTitle = ""; // no css locator
+        String cssShopPhoneNumber = ""; // no css locator
         String cssContactTitle = "div#contact-link";
         String cssSignInTitle = "div.header_user_info";
 
@@ -34,15 +34,17 @@ public class Main {
         String xpathTab1Women = "(//a[@class='sf-with-ul'])[1]";
         String xpathTab2Dresses = "(//a[@class='sf-with-ul'])[4]";
         String xpathTab3Tshirts = "//ul[contains(@class,'sf-menu')]/li[3]";
-        String cssTab1Women = "";
-        String cssTab2Dresses = "";
-        String cssTab3Tshirts = "";
+        String cssTab1Women = "ul.sf-menu>li:nth-child(1)";
+        String cssTab2Dresses = "ul.sf-menu>li:nth-child(2)";
+        String cssTab3Tshirts = "ul.sf-menu>li:nth-child(3)";
 
         String xpathCrumbsHome = "//a[@class='home']";
-//        String xpathCrumbsFirst = "//a[@class='home']/following-sibling::a";
         String xpathCrumbsFirst = "//div[contains(@class,'breadcrumb')]/a[2]";
         String xpathCrumbsSecond = "//span[@class='navigation-pipe']//text()";
-//        String xpathCrumbsSecond = "//span[@class='navigation-pipe']//text()";
+        //
+        String cssCrumbsHome = "div.breadcrumb>a:nth-child(1)";
+        String cssCrumbsFirst = "div.breadcrumb>a:nth-child(3)";
+        String cssCrumbsSecond = "div.breadcrumb>a:nth-child(5)";
 
         String xpathAuthTitle = "//h1";
         String xpathCreateAccountTitle = "//form[@id='create-account_form']/h3";
@@ -50,6 +52,13 @@ public class Main {
         String xpathNewEmailTitle = "//label[@for='email_create']";
         String xpathNewEmailField = "//input[@id='email_create']";
         String xpathCreateAccountButton = "//button[@id='SubmitCreate']";
+        //
+        String cssAuthTitle = "h1";
+        String cssCreateAccountTitle = "#create-account_form>h3";
+        String cssNewEmailHint = "#create-account_form p";
+        String cssNewEmailTitle = "label[for='email_create']";
+        String cssNewEmailField = "label[for='email_create'] + input";
+        String cssCreateAccountButton = "button#SubmitCreate";
 
         String xpathRegisteredTitle = "//form[@id='login_form']/h3";
         String xpathRegEmailTitle = "//label[@for='email']";
@@ -58,6 +67,15 @@ public class Main {
         String xpathRegPasswordField = "//input[@id='passwd']";
         String xpathForgotLink = "//p[contains(@class,'lost_password')]/a";
         String xpathSingInButton = "//button[@id='SubmitLogin']";
+        //
+        String cssRegisteredTitle = "#login_form>h3";
+        String cssRegEmailTitle = "#login_form label[for='email']";
+        String cssRegEmailField = "#login_form label[for='email'] + input";
+        String cssRegPasswordTitle = "#login_form label[for='passwd']";
+        String cssRegPasswordField = "#login_form label[for='passwd'] + input";
+        String cssRegForgotLink = "#login_form p.lost_password a";
+        String cssSignInButton = "#SubmitLogin";
+
 
     }
 }
